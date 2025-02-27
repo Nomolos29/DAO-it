680304aa5667fe308ae76a2552318e3f8e05871a
Iteoluwakisi Bello <iteoluwakisibello@Iteoluwakisis-MacBook-Pro.local>
2025-02-27 21:39:17 +0100

    feat: Integrate Thirdweb SDK and add wallet connection functionality

---
+++ components/walletButton.tsx
@@ -0,0 +1,46 @@
+import { createThirdwebClient } from "thirdweb";
+import { ConnectButton, lightTheme } from "thirdweb/react";
+
+import { inAppWallet, createWallet } from "thirdweb/wallets";
+
+const clientId =
+  process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "defaultClientId";
+const client = createThirdwebClient({ clientId });
+
+const wallets = [
+  inAppWallet({
+    auth: {
+      options: [
+        "google",
+        "telegram",
+        "farcaster",
+        "email",
+        "x",
+        "passkey",
+        "phone",
+      ],
+    },
+  }),
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("me.rainbow"),
+  createWallet("io.rabby"),
+  createWallet("io.zerion.wallet"),
+];
+
+const WalletButton = () => {
+  return (
+    <ConnectButton
+      client={client}
+      wallets={wallets}
+      theme={lightTheme({
+        colors: {
+          primaryButtonBg: "#FEE539",
+        },
+      })}
+      connectModal={{ size: "compact" }}
+    />
+  );
+};
+
+export default WalletButton;
