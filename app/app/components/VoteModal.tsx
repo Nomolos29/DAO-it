56c47ee6b6d3f17918af81d379f0e269b6a21445
Belloabraham121 <belloabraham121@gmail.com>
2025-04-30 17:40:22 +0100

    chore: clean up code and update tsconfig schema
    Remove unused imports and commented code in Header.tsx and VoteModal.tsx. Add $schema to tsconfig.json for better configuration management.

--- app/app/components/VoteModal.tsx
+++ app/app/components/VoteModal.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import Modal, { ModalProps } from './Modal'
+// import React, { useState } from 'react'
+import Modal, { ModalProps } from "./Modal";
 
 // interface VoteModalProps extends ModalProps {
 //     // totalVotes: number;
@@ -9,30 +9,38 @@ import Modal, { ModalProps } from './Modal'
 //     // voters: string[];
 // }
 
-const VoteModal: React.FC<ModalProps> = ({isOpen, onClose}) => {
-  const [votes, setVotes] = useState<number>(1);
+const VoteModal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+  // const [votes, setVotes] = useState<number>(1);
 
-  const pricePerVote:number = 5;
+  // const pricePerVote:number = 5;
 
-  const voteButton = "w-full h-[50px] rounded-xl px-[14px] text-md font-medium border border-[#E0E2EA] flex items-center";
-  
+  const voteButton =
+    "w-full h-[50px] rounded-xl px-[14px] text-md font-medium border border-[#E0E2EA] flex items-center";
+
   return (
     <Modal isOpen={isOpen} bgBlured onClose={onClose}>
-        <main className='w-[450px] flex flex-col gap-y-6'>
-          <h3 className='text-xl font-medium'>Book Exchange Initiative Exchange Initiative</h3>
+      <main className="w-[450px] flex flex-col gap-y-6">
+        <h3 className="text-xl font-medium">
+          Book Exchange Initiative Exchange Initiative
+        </h3>
 
-          <div className='w-full flex flex-col gap-y-[14px]'>
-            <span className={`${voteButton}`}> Vote Yes</span>
-            <span className={`${voteButton}`}>Vote No</span>
-            <span className={`${voteButton}`}>Vote Abstain</span>
-          </div>
+        <div className="w-full flex flex-col gap-y-[14px]">
+          <span className={`${voteButton}`}> Vote Yes</span>
+          <span className={`${voteButton}`}>Vote No</span>
+          <span className={`${voteButton}`}>Vote Abstain</span>
+        </div>
 
-          <p>Proposal ID: A100</p>
+        <p>Proposal ID: A100</p>
 
-          <button type='button' className='bg-[#1D54E1] text-white w-full h-[54px] flex justify-center items-center rounded-lg'>Submit your vote</button>
-        </main>
+        <button
+          type="button"
+          className="bg-[#1D54E1] text-white w-full h-[54px] flex justify-center items-center rounded-lg"
+        >
+          Submit your vote
+        </button>
+      </main>
     </Modal>
-  )
-}
+  );
+};
 
-export default VoteModal
+export default VoteModal;
