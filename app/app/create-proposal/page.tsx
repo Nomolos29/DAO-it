d43f0a4294dfadf2e826fe6b215d15199ef377e6
Sunday Solomon <nomolos2019@gmail.com>
2025-05-05 00:41:54 +0100

    Merging

--- app/app/create-proposal/page.tsx
+++ app/app/create-proposal/page.tsx
@@ -13,7 +13,6 @@ import { Modal } from "../components";
 import { useCreateProposal } from "../hooks/useCreateProposal";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import openai from "../lib/openai";
-import { FaArrowLeftLong } from "react-icons/fa6";
 
 // Types definitions
 interface ProposalState {
